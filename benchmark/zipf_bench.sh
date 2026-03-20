5926b1fc504b7c294749e4910885f909e28b1291
ccrosie-wq <ccrosie@bu.edu>
2026-03-20 13:01:32 -0400

    add buffer size as param to benchmarking script"

--- benchmark/zipf_bench.sh
+++ benchmark/zipf_bench.sh
@@ -6,14 +6,16 @@ SCALE_FACTOR="1000"
 read_weight=1
 update_weight=1
 time=10
+buffer_size=128
 alpha=1.5
 
-while getopts ":r:u:t:a:" opt; do
+while getopts ":r:u:t:a:b:" opt; do
   case $opt in
     u) update_weight="$OPTARG" ;;
     r) read_weight="$OPTARG" ;;
     t) time="$OPTARG" ;;
     a) alpha="$OPTARG" ;;
+    b) buffer_size="$OPTARG" ;;
     \?) echo "Invalid option: -$OPTARG" >&2
         echo "Usage:"
         echo "    -r read_weight (default 1)"
@@ -24,6 +26,9 @@ while getopts ":r:u:t:a:" opt; do
         ;;
   esac
 done
+
+# set buffer size
+./set_buffersize -m "$buffer_size"
 
 # apply alpha
 tmp=$(mktemp)
